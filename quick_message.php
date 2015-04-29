794082ee859b003b7c9c3769f7474ee195a57ac6
Ruby Zheng <zhengsan@gmail.com>
2015-04-28 20:52:26 -0400

    added form submission

--- quick_message.php
+++ quick_message.php
@@ -1,28 +1,48 @@
-<?php
-if(isset($_POST["submit"])){
-// Checking For Blank Fields..
-    if($_POST["email"]==""||$_POST["email"]==""||$_POST["message"]==""){
-        echo "Fill All Fields..";
-    }else{
-// Check if the "Sender's Email" input field is filled out
-        $email=$_POST['email'];
-// Sanitize E-mail Address
-        $email =filter_var($email, FILTER_SANITIZE_EMAIL);
-// Validate E-mail Address
-        $email= filter_var($email, FILTER_VALIDATE_EMAIL);
-        if (!$email){
-            echo "Invalid Sender's Email";
+---
+layout: default
+title: Message Sent
+---
+{% include navbar-connect.html %}
+
+<div class="container">
+    <?php
+        echo var_dump($_POST);
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Checking For Blank Fields...
+            if($_POST['email']==""||$_POST['subject']==""||$_POST['message']==""){
+                echo '<div class="content">
+                        <h1>Uh-oh</h1>
+                        <p class="lead">Your message was not sent. Please re-submit and make sure all fields are entered.</p>
+                    </div>';
+            }
+            else{
+                // Check if the "Sender's Email" input field is filled out
+                $email=$_POST['email'];
+                // Sanitize E-mail Address
+                $email =filter_var($email, FILTER_SANITIZE_EMAIL);
+                // Validate E-mail Address
+                $email= filter_var($email, FILTER_VALIDATE_EMAIL);
+                if (!$email){
+                    echo '<div class="content">
+                        <h1>Uh-oh</h1>
+                        <p class="lead">Your message was not sent. Please re-submit and make sure your email is accurately entered.</p>
+                        </div>';
+                }
+                else{
+                    $subject = $_POST['subject'];
+                    $message = $_POST['message'];
+                    $headers = 'From:'. $email . "\r\n"; // Sender's Email
+                    // Message lines should not exceed 70 characters (PHP rule), so wrap it
+                    $message = wordwrap($message, 70);
+                    // Send Mail By PHP Mail Function
+                    mail('zhengsan@gmail.com', $subject, $message, $headers);
+
+                    echo '<div class="content">
+                        <h1>Uh-oh</h1>
+                        <p class="lead">Your message has been sent! Thanks for visiting and chat soon!</p>
+                    </div>';
+                }
+            }
         }
-        else{
-            $subject = $_POST['email'];
-            $message = $_POST['message'];
-$headers = 'From:'. $email . "\r\n"; // Sender's Email
-// Message lines should not exceed 70 characters (PHP rule), so wrap it
-$message = wordwrap($message, 70);
-// Send Mail By PHP Mail Function
-mail("zhengsan@gmail.com", $subject, $message, $headers);
-header("Location: confirmation.html");
-}
-}
-}
-?>
+    ?>
+</div>
